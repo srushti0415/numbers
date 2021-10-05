ee7ed3b0695ee5faef04e9ad783d58f72cafc000
srushti0415 <srushtikulkarni15@gmail.com>
2021-10-05 20:59:51 +0530

    greater number

---
+++ index.js
@@ -0,0 +1,15 @@
+function myfunction(a,b,c,d){
+    if (a>b && a>c && a>d){
+        console.log("a is max no")
+    }
+    else if (b>a && b>c && b>d){
+        console.log("b is max")
+    }
+    else if (c>a && c>b && c>d){
+        console.log("c is max")
+    }
+    else {
+        console.log("d is max")
+    }
+}
+myfunction(2,4,8,7)
